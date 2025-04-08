0bca812d8bbb8f92476ef034066c5493b6684dee
Aman <meamanx7@gmail.com>
2025-04-08 10:45:35 +0530

    added ans tracking

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ turtle.shape(img)
 
 data = pandas.read_csv("50_states.csv")
 all_state = data.state.to_list()
+guessed_state = []
+while len(guessed_state) < 50:
+    ans_state = screen.textinput(title=f"{len(guessed_state)}/50 correct", prompt="What the state's name?")
+    print(ans_state)
 
-ans_state = screen.textinput(title="Give it a guess", prompt="What the state's name?")
-print(ans_state)
 
-
-if ans_state in all_state:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state = data[data.state == ans_state]
-    t.goto(state.x.item(),state.y.item())
-    t.write(ans_state)
+    if ans_state in all_state:
+        guessed_state.append(ans_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state = data[data.state == ans_state]
+        t.goto(state.x.item(),state.y.item())
+        t.write(ans_state)
 
 screen.exitonclick()
